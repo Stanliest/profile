a20d07bc1265410458c69eaf841fdedc5d16584c
Stanley Liu <stanley.liu@ibm.com>
2019-05-01 14:14:42 -0400

    typo

--- src/text.js
+++ src/text.js
@@ -2,6 +2,6 @@ module.exports = {
   Intro: {
     About: "Hi, my name is Stanley Liu. I'm from Kunming, China. I came to Canada with my family when I was 17.",
     School: "I am studying software engineering at McMaster University. I was going to choose civil engineering in high school, but I thought programming was also very interesting.",
-    Interest: "I watch all kinds of sports including basketball, tennis, soccer, etc. I play tennis a lot during summer. I also likes to play competitive video games on PC and phone."
+    Interest: "I watch all kinds of sports including basketball, tennis, soccer, etc. I play tennis a lot during summer. I also like to play competitive video games on PC and phone."
   }
 }
